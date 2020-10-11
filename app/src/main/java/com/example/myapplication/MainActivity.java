bad543f33c2216b622f56285988746cb33a30cd9
vasurao4321 <64043935+vasurao4321@users.noreply.github.com>
2020-10-11 12:20:51 -0700

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * Skeleton of an Android Things activity.
+ * <p>
+ * Android Things peripheral APIs are accessible through the PeripheralManager
+ * For example, the snippet below will open a GPIO pin and set it to HIGH:
+ * <p>
+ * PeripheralManager manager = PeripheralManager.getInstance();
+ * try {
+ * Gpio gpio = manager.openGpio("BCM6");
+ * gpio.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW);
+ * gpio.setValue(true);
+ * } catch (IOException e) {
+ * Log.e(TAG, "Unable to access GPIO");
+ * }
+ * <p>
+ * You can find additional examples on GitHub: https://github.com/androidthings
+ */
+class MainActivity {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+    }
+}
